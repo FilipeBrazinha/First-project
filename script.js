554f7a2e85f7f1fa05f88cf3473afb06fe8f3325
FilipeBrazinha <98277066+FilipeBrazinha@users.noreply.github.com>
2022-02-19 17:11:07 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  var css = document.querySelector("h3");
+  var color1 = document.querySelector(".color1");
+  var color2 = document.querySelector(".color2");
+  var body = document.getElementById("gradient")
+
+  function setGradient(){
+    body.style.background =
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+  }
+  color1.addEventListener("input", setGradient);
+  color2.addEventListener("input", setGradient);
+  
+
+
